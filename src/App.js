b01b7b9fbf535ee06226713f6c3bbbdfe2b1692b
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-08-04 16:35:53 -0700

    Added drop down filter

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ import Header from './Header.js';
 import ImageList from './ImageList.js'
 import { images} from './data.js';
 
+let keywords = images.map(image => {
+  const container = [];
+  container.push(image.keyword)
+  return container;
+})
 
 class App extends React.Component {
+  state = {
+    filter: 'rhino'
+  }
+  handleKeyword = (e) => {
+    const keyword = e.target.value;
+    this.setState({ filter: keyword })
+  }
   render() {
+
+    const filteredImages = images.filter(image => image.keyword === this.state.filter)
 
     return (
       <>
-      <Header />
-      <ImageList images={images} />
+        <Header />
+        <select onChange={this.handleKeyword} name='' id=''>
+          {
+            keywords.map(keyword => <option key={keyword} value={keyword}>{keyword}</option>)
+          }
+        </select>
+        <ImageList images={filteredImages} />
       </>
     );
   }
